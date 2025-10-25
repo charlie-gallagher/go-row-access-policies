53f337ebe93c7de530fe310740c1536ef9bff9d7
Charlie Gallagher <charlie@ixisdigital.com>
2025-10-25 12:11:37 -0400

    test: refactor test strings for clarity

--- main_test.go
+++ main_test.go
@@ -27,7 +27,7 @@ func TestPolicyItemConvertsToJson(t *testing.T) {
 	t.Run("non-empty policy item", func(t *testing.T) {
 		pi := PolicyItem{Column: "charlie", Values: []string{"one", "two", "three"}}
 		got := pi.ToJson()
-		want := "{\"column\":\"charlie\",\"values\":[\"one\",\"two\",\"three\"]}"
+		want := `{"column":"charlie","values":["one","two","three"]}`
 		if got != want {
 			t.Fail()
 		}
@@ -46,7 +46,7 @@ func TestPolicyConvertsToJson(t *testing.T) {
 	t.Run("non-empty policy", func(t *testing.T) {
 		p := Policy{Role: "admin", Policy: []PolicyItem{{Column: "Region", Values: []string{"one", "two", "three"}}}}
 		got := p.ToJson()
-		want := "{\"role\":\"admin\",\"policy\":[{\"column\":\"Region\",\"values\":[\"one\",\"two\",\"three\"]}]}"
+		want := `{"role":"admin","policy":[{"column":"Region","values":["one","two","three"]}]}`
 		if got != want {
 			t.Fail()
 		}
@@ -56,7 +56,7 @@ func TestPolicyConvertsToJson(t *testing.T) {
 func TestValidateConfigWorks(t *testing.T) {
 	t.Run("valid config", func(t *testing.T) {
 		err := ValidateConfig([]byte(
-			"{\"policies\":[{\"role\":\"admin\", \"policy\":[{\"column\":\"Region\", \"values\":[\"one\",\"two\"]}]}]}",
+			`{"policies":[{"role":"admin", "policy":[{"column":"Region", "values":["one","two"]}]}]}`,
 		))
 		if err != nil {
 			t.Fail()
@@ -70,7 +70,7 @@ func TestValidateConfigWorks(t *testing.T) {
 	})
 
 	t.Run("Empty set of policies is ok", func(t *testing.T) {
-		err := ValidateConfig([]byte("{\"policies\":[]}"))
+		err := ValidateConfig([]byte(`{"policies":[]}`))
 		if err != nil {
 			t.Fail()
 		}
@@ -86,7 +86,7 @@ func TestValidateConfigFails(t *testing.T) {
 	})
 	t.Run("Invalid config", func(t *testing.T) {
 		err := ValidateConfig([]byte(
-			"{\"policies\":[{\"oops\":\"admin\", \"policy_items\":[{\"column\":\"Region\", \"values\":[\"one\",\"two\"]}]}]}",
+			`{"policies":[{"oops":"admin", "policy_items":[{"column":"Region", "values":["one","two"]}]}]}`,
 		))
 		if err == nil {
 			t.Fail()
@@ -153,7 +153,6 @@ func getDbHandle() (*sql.DB, error) {
 
 	return db, nil
 }
-
 
 func fetchOneRow(db *sql.DB, query string, dest ...any) error {
 	rows, err := db.Query(query)
